b8c9911dacaba604b0e7382a67f20b4ec7c8e740
swatheesh <swatheeshm@gmail.com>
2020-04-16 17:42:44 +0530

    Changed wsgi2

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'Portfolio/static/')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
