9252d9f14bd53b21be6cedc370f0c0c4edb0c134
swatheesh <swatheeshm@gmail.com>
2020-04-16 10:26:52 +0530

    newcommit

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'portfolio/static/')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'portfolio/staticfiles')
 STATIC_URL = '/staticfiles/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
